66ea983ff80081475afd8b21c6371132d5c4d258
SHAMSHAD7643 <shaim7643@gmail.com>
2026-03-03 21:26:28 +0530

    ternary operator

---
+++ Ternary-operator/index.js
@@ -0,0 +1,15 @@
+// let result;
+
+// if (age >= 18) {
+//   result = "Adult";
+// } else {
+//   result = "Minor";
+// } 
+
+
+
+let age = 18;
+
+let result = age >= 18 ? "Adult" : "Minor";
+
+console.log(result);
